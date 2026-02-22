1e35106bc0bb02bfe3b197e55ad00c63e19eabda
jestrada5-arch <jestrada5@miumg.edu.gt>
2026-02-22 11:42:20 -0600

    Primer commit, Version funcional de Persona

---
+++ Persona.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+class Persona {
+    
+    protected: 
+        string cui, nombres, apellidos, direccion, fecha_nacimiento;
+        int telefono;
+        bool genero;
+
+    protected:
+        
+        Persona() {}
+        Persona(string c, string nom, string ape, string dir, int tel, string fn, bool gen) {
+            cui = c;
+            nombres = nom;
+            apellidos = ape;
+            direccion = dir;
+            telefono = tel;
+            fecha_nacimiento = fn;
+            genero = gen;
+        }
+
+       
+        void crear();
+        void leer();
+        void actualizar();
+        void borrar();
+};
